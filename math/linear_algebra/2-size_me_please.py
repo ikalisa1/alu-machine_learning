64f5d0e24cc0ddce7d043e4f8dacc73c15470186
ikalisa <ikalisa1@alustudent.com>
2025-01-24 09:03:21 -0900

    2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
-matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-the_middle = []
-for row in matrix:
-        the_middle.append(row[2:4])
-        print("The middle columns of the matrix are: {}".format(the_middle))
+"""
+matrix_shape = __import__('2-size_me_please').matrix_shape
+
+mat1 = [[1, 2], [3, 4]]
+print(matrix_shape(mat1))
+mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+        print(matrix_shape(mat2))
+        """
+
+
+        def matrix_shape(matrix):
+                """Calculates the shape of a matrix.
+
+                    Args:
+                            matrix: The matrix to calculate the shape of.
+
+                                Returns:
+                                        A list containing the shape of the matrix in each dimension.
+                                        """
+
+                                            shape = []
+                                                while isinstance(matrix, list):
+                                                            shape.append(len(matrix))
+                                                                    matrix = matrix[0]  # Move one level deeper into the matrix
+                                                                        return shape  # Return the shape after calculating all dimensions
+
