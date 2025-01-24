86f4971da864083134a69a82f91a8dc723e5e54e
ikalisa1 <ikalisa1@alustudent.com>
2025-01-24 11:00:10 -0800

    3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -8,6 +8,6 @@ def matrix_transpose(matrix):
     """
     transpose = [
         [matrix[j][i] for j in range(len(matrix))]
-            for i in range(len(matrix[0]))
-    ]         
+        for i in range(len(matrix[0]))
+    ]
     return transpose
