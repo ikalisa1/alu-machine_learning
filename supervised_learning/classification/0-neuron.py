44f5bb8fd64a4a05a9139f61039fe6ca5e58147b
i.kalisa1 <i.kalisa1@alustudent.com>
2025-05-18 09:22:29 -0800

    Neuron

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,19 +1,22 @@
 #!usr/bin/env python3
 """Neuron class for binary classification"""
 import numpy as np
+
 class Neuron:
-    def_init_(self, nx):
+    """class that defines a single neuron for binary classification"""
+
+    def __init__(self, nx):
    
 
-    if not isinstance(nx, int):
-        raise TypeError("nx must bn integer")
+         if not isinstance(nx, int):
+            raise TypeError("nx must bn integer")
 
-if nx < 1:
-    raise ValueError("nx must be a positive integer")
+         if nx < 1:
+            raise ValueError("nx must be a positive integer")
 
 #innitialize weights with a randon nomarl distribution
-self.W = np.random.rand(1, nx)
+         self.W = np.random.rand(1, nx)
 
 #initialize bias and activated output to 0
-self.b = 0
-self.A = 0
+         self.b = 0
+         self.A = 0
