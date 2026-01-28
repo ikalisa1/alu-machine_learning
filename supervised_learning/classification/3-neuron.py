Fix cost calculation formula in 3-neuron.py

supervised_learning/classification/3-neuron.py
@@ -80,6 +80,6 @@ class Neuron:
             float: The cost of the model.
         """
         m = Y.shape[1]
-        cost = -1/m * np.sum(Y * np.log(A) + (1.0000001 - Y) *
+        cost = -1/m * np.sum(Y * np.log(A) + (1 - Y) *
                              np.log(1.0000001 - A))
         return cost