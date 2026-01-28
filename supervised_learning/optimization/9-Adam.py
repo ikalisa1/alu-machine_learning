4e8a9a8702ad993558e7544544061ac4a00bf434
Administrator <admin@example.com>
2026-01-28 00:44:16 -0800

    Fix pycodestyle W293 errors in 9-Adam.py - Remove whitespace from blank lines

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -23,15 +23,15 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """
     # Update first moment (momentum)
     v_new = beta1 * v + (1 - beta1) * grad
-    
+
     # Update second moment (RMSProp)
     s_new = beta2 * s + (1 - beta2) * np.square(grad)
-    
+
     # Bias correction
     v_corrected = v_new / (1 - np.power(beta1, t))
     s_corrected = s_new / (1 - np.power(beta2, t))
-    
+
     # Update variable
     var_updated = var - alpha * v_corrected / (np.sqrt(s_corrected) + epsilon)
-    
+
     return var_updated, v_new, s_new
