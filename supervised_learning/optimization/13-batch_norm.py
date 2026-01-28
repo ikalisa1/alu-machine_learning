713d7946f77400b91235016da17c049b4d59ae7a
Administrator <admin@example.com>
2026-01-28 00:52:09 -0800

    Add 13-batch_norm.py - Batch normalization

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module for batch normalization."""
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """
+    Normalize an unactivated output of a neural network using batch normalization.
+
+    Args:
+        Z: numpy.ndarray of shape (m, n) that should be normalized
+        gamma: numpy.ndarray of shape (1, n) containing the scales
+        beta: numpy.ndarray of shape (1, n) containing the offsets
+        epsilon: A small number used to avoid division by zero
+
+    Returns:
+        The normalized Z matrix
+    """
+    mean = np.mean(Z, axis=0, keepdims=True)
+    variance = np.var(Z, axis=0, keepdims=True)
+    Z_norm = (Z - mean) / np.sqrt(variance + epsilon)
+    Z_output = gamma * Z_norm + beta
+    return Z_output
